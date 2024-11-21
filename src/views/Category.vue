c76b197b17aa67369cdab9a12d5f8d1432ed3df2
ilyasdx <ilyasdx91@gmail.com>
2024-11-21 20:05:26 +0500

    checkOrientation

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -6,6 +6,11 @@
   <div class="category">
     <img src="@/assets/img/image-category.png" alt="" />
     <div class="inner">
+      <!-- <pre>{{ accelerometer }}</pre> -->
+      <pre>IsStarted: {{ accelerometer.isStarted }}</pre>
+      <pre>X: {{ accelerometer.x }}</pre>
+      <pre>Y: {{ accelerometer.y }}</pre>
+      <pre>Z: {{ accelerometer.z }}</pre>
       <h1>Sport trophies</h1>
       <p>
         Describe sport trophies. Just make sure your friends guess what trophy
@@ -19,6 +24,29 @@
 </template>
 
 <script setup>
-import { Alert } from 'vue-tg'
+import { ref, onMounted, onBeforeUnmount, computed } from 'vue'
 import CategoryHeader from '@/components/CategoryHeader.vue'
+
+const tg = window.Telegram.WebApp
+
+// Accelerometer
+// const user = ref({ name: 'John', age: 30 }); // Создает реактивную ссылку на объект
+// console.log(user.value.name); // Выведет 'John'
+
+//const accelerometer = ref({ X: tg.Accelerometer }) // Создает реактивную ссылку на объект
+//const accelerometerIsStarted = ref(tg.Accelerometer.isStarted)
+//const accelerometerX = ref(tg.Accelerometer.x)
+//const accelerometerY = ref(tg.Accelerometer.y)
+//const accelerometerZ = ref(tg.Accelerometer.z)
+
+const accelerometer = computed(() => {
+  return tg.Accelerometer
+})
+onMounted(() => {
+  tg.Accelerometer.start()
+  console.log(tg.Accelerometer.isStarted)
+  console.log(tg.Accelerometer.x)
+  console.log(tg.Accelerometer.y)
+  console.log(tg.Accelerometer.z)
+})
 </script>
