6ad127c9b627b110abd05b56170e67512edba04d
umid <umidjonsmail@gmail.com>
2024-11-26 20:19:05 +0500

    changes

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,21 @@
   <router-view></router-view>
 </template>
 
-<script></script>
+<script setup>
+import { onMounted, onUnmounted } from 'vue'
+
+let wakeLock = null
+
+onMounted(async () => {
+  try {
+    wakeLock = await navigator.wakeLock.request('screen')
+  } catch (err) {
+    console.log(err)
+  }
+})
+onUnmounted(() => {
+  wakeLock.release().then(() => {
+    wakeLock = null
+  })
+})
+</script>
